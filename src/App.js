940c06dfbf2a3ee6f514752447f22ed2433b702f
Redstone <redstoundel@gmail.com>
2021-10-23 17:06:52 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from "react";
+import { Route, Switch } from "react-router";
+import Dropdown from "./components/Dropdown";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages";
+import About from "./pages/about";
+import Menu from "./pages/menu";
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if(window.innerWidth > 768 && isOpen) setIsOpen(false);
+    }
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    }
+  });
+
+  return (
+    <>
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/about" component={About} />
+      </Switch>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
